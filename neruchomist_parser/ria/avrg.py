8e40a893979fc7cfdb801cf5fae93c4b68db47d5
Viacheslav Shchurov <diskoknife@gmail.com>
2023-04-27 21:27:38 +0000

    fix: class method turned into a function

--- neruchomist_parser/ria/avrg.py
+++ neruchomist_parser/ria/avrg.py
@@ -1,3 +1,5 @@
+import requests
+
 def get_average_price(
         category: int, 
         sub_category: int, 
@@ -8,5 +10,20 @@ def get_average_price(
         date_from=-1, 
         date_to=-1, 
         api_token="", 
-        url = "https://developers.ria.com/dom/average_price?"):
-    pass
+        url = "https://developers.ria.com/dom/average_price?"
+        ):
+    final_url = url + "category=" + str(category) + "&sub_category=" + str(sub_category) + "&operation=" + str(operation)
+    if state_id >= 1:
+        final_url += "&state_id="
+    if city_id >= 1:
+        final_url += "&city_id=" + str(city_id)
+    if district_id >= 1:
+        final_url += "&district_id=" + str(district_id)
+    if date_from != "":
+        final_url += "&date_from=" + date_from
+    if date_to != "":
+        final_url += "&date_to=" + date_to
+    final_url += "&api_key=" + api_token
+    r = requests.get(final_url)
+    r.json()
+    return r
